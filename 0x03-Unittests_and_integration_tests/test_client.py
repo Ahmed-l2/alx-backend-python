21743f9811b4d076f79c5a7804f884f5b91ece5d
Ahmed <ahmed_l2@yahoo.com>
2024-07-30 03:07:07 +0300

    6. More patching

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -21,13 +21,27 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_get_json.assert_called_once()
 
     def test_public_repos_url(self):
-        """test for GithubOrgClient._public_repos_url method"""
+        """Test for GithubOrgClient._public_repos_url method"""
         with patch('client.GithubOrgClient.org',
                    new_callable=PropertyMock) as mock_org:
             mock_org.return_value = {"repos_url": "mocked_url"}
             client = GithubOrgClient("org")
             result = client._public_repos_url
             self.assertEqual(result, "mocked_url")
+
+    @patch('client.get_json', return_value=[{"name": "value1"},
+                                            {"name": "value2"}])
+    def test_public_repos(self, mocked_get_json):
+        """Test for GithubOrgClient._public_repos method"""
+        with patch('client.GithubOrgClient._public_repos_url',
+                   new_callable=PropertyMock) as mock_public_repos_url:
+            mock_public_repos_url.return_value = "mock_url"
+            client = GithubOrgClient('org')
+            result = client.public_repos()
+
+            self.assertAlmostEqual(result, ['value1', 'value2'])
+            mocked_get_json.assert_called_once()
+            mock_public_repos_url.assert_called_once()
 
 
 if __name__ == '__main__':
